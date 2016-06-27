bbf33ef8a450cc1b25f6e57455f0b8129ca587b0
Jack Oddy <jackoddy91@gmail.com>
2016-06-27 19:38:02 +0100

    adds formatting changes

--- lib/main.rb
+++ lib/main.rb
@@ -30,25 +30,27 @@ end
 
 get ('/') do
 erb :layout do
-  "<h1> #{fizzbuzz '15'} </h1>
+  "<h1><font size='7'>#{fizzbuzz '15'}</font></h1>
     <p> Please enter a number to fizzbuzz it.</p>
     <br>
     <form action = '/' method='post'>
       <input type= 'text' name ='number'>
       <input type ='submit'>
+      <input type ='reset'>
     </form>"
   end
 end
 
 post ('/') do
   erb :layout do
-    "<h1> #{fizzbuzz '15'} </h1>
+    "<h1><font size='7'>#{fizzbuzz '15'}</font></h1>
     <p> #{params[:number]} --> #{fizzbuzz params[:number]}</p>
     <p> Please enter a number to fizzbuzz it.</p>
     <br>
     <form action = '/' method='post'>
       <input type= 'text' name ='number'>
       <input type ='submit'>
+      <input type ='reset'>
     </form>"
   end
 end
