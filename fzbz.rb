2943cd7ef03186da9ad308dc06bd1276e0aaf2e9
Jack Oddy <jackoddy91@gmail.com>
2016-06-27 14:11:55 +0100

    adds fizzbuzz and erb files

---
+++ fzbz.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+
+class Integer
+
+  def fizzbuzz
+    str = ''
+    str << 'fizz' if self % 3 == 0
+    str << 'buzz' if self % 5 == 0
+    str == '' ? self : str
+  end
+
+end
+
+
+get ('/') do
+erb :layout do
+  "<h1> #{15.fizzbuzz} </h1>
+    <p> Please enter a number to fizzbuzz it.</p>
+    <br>
+    <form action = '/' method='post'>
+      <input type= 'text' name ='number'>
+      <input type ='submit'>
+    </form>>"
+  end
+end
+
+post ('/') do
+  erb :layout do
+    "<h1> #{15.fizzbuzz} </h1>
+    <p> #{params[:number]} --> #{params[:number].to_i.fizzbuzz}</p>"
+  end
+end
