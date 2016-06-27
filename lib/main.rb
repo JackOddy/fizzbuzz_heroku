033f8f6073156be9cb8eab440e1bfab1494105e7
Jack Oddy <jackoddy91@gmail.com>
2016-06-27 21:19:25 +0100

    refactors and adds general erb

--- lib/main.rb
+++ lib/main.rb
@@ -29,28 +29,13 @@ def fizzbuzz(str)
 end
 
 get ('/') do
-erb :layout do
-  "<h1><font size='7'>#{fizzbuzz '15'}</font></h1>
-    <p> Please enter a number to fizzbuzz it.</p>
-    <br>
-    <form action = '/' method='post'>
-      <input type= 'text' name ='number'>
-      <input type ='submit'>
-      <input type ='reset'>
-    </form>"
-  end
+erb :fzbz do
+  "fizz --> buzz"
+end
 end
 
 post ('/') do
-  erb :layout do
-    "<h1><font size='7'>#{fizzbuzz '15'}</font></h1>
-    <p> #{params[:number]} --> #{fizzbuzz params[:number]}</p>
-    <p> Please enter a number to fizzbuzz it.</p>
-    <br>
-    <form action = '/' method='post'>
-      <input type= 'text' name ='number'>
-      <input type ='submit'>
-      <input type ='reset'>
-    </form>"
+  erb :fzbz do
+    "#{params[:number]} --> #{fizzbuzz params[:number]}"
   end
 end
